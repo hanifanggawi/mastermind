7a2304d9a66300d82cc21f0baf5d4d73496ab66a
hanifanggawi <hanifanggawi45@gmail.com>
2022-08-07 18:07:16 +0700

    feat: add game result in sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+import { get } from "svelte/store";
+
+import { getAlertMessage, restartGame } from "../game";
+import { GameState } from "../game/constants";
+import { gameState, guesses } from "../store";
 </script>
 
 <div class="sidebar">
-  <h1>Master<span class="orange">Mind</span> </h1>
   <div class="instructions">
+    <h1>Master<span class="orange">Mind</span> </h1>
     <h4>How To Play</h4>
     <ul>
       <li>
@@ -26,10 +31,24 @@
       </li>
     </ul>
   </div>
+  {#if $gameState !== GameState.Playing}
+     <div class="game-result">
+       <h3 class="result-message">{getAlertMessage(get(gameState))}</h3>
+       <div class="guesses">Guesses: {get(guesses).length}</div>
+       <div class="buttons">
+         <button on:click={restartGame}>
+           Play Again
+         </button>
+       </div>
+     </div>
+  {/if}
 </div>
 
 <style>
   .sidebar {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     background-color: rgb(28, 29, 51);
     width: 100%;
     height: 100%;
@@ -42,6 +61,31 @@
     color: #fe9d28
   }
 
+  .game-result {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    padding: 1em;
+    row-gap: 0.5em;
+  }
+
+  button {
+    padding: 0.7em 1.5em;
+    border-radius: 5px;
+    border: none;
+    background: #fe9d28;
+    font-weight: 600;
+    margin-top: 1em;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background: #ffaa41;
+  }
+
   ul {
     padding: 1em;
   }
@@ -52,11 +96,11 @@
     list-style-type: circle;
   }
 
-  h1, h4{
+  h1, h3, h4{
     text-align: center;
   }
 
-  h4, ul {
+  h3, h4, ul {
     margin: 0;
   }
 </style>
